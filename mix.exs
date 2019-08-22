06b478c5af73bdf9a7ef1025d7588009be5aec8a
lorenzosinisi <lasslo.net@gmail.com>
2019-08-22 16:03:40 +0200

    Add package meta

--- mix.exs
+++ mix.exs
@@ -1,25 +1,42 @@
-defmodule UuidTools.MixProject do
+defmodule UUIDTools.MixProject do
   use Mix.Project
-
+  @version "0.1.0"
   def project do
     [
       app: :uuid_tools,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.9",
-      start_permanent: Mix.env() == :prod,
+      docs: [extras: ["README.md"], main: "readme", source_ref: "v#{@version}"],
+      source_url: "https://github.com/lorenzosinisi/uuid_tools",
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
+    []
+  end
+
+  defp deps do
     [
-      extra_applications: [:logger]
+      {:ex_doc, "~> 0.16", only: :dev},
+      {:earmark, "~> 1.2", only: :dev}
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
-    []
+  defp description do
+    """
+    Tools for UUID generator and utilities aroud it for Elixir.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Lorenzo Sinisi"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/lorenzosinisi/uuid_tools"}
+    ]
   end
 end
